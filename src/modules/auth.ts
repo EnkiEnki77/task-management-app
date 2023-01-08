1f5b2d86b29399384926263ed5448180c5b76084
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2023-01-07 22:20:04 -0800

    created middleware to protect routes from users without a proper token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -15,3 +15,32 @@ export const hashedPassword = (password: string) => {
 export const compareHashed = (password: string, hash: string) => {
     return bcrypt.compare(password, hash)
 }
+
+export const protect = (req, res, next) => {
+    const bearer = req.headers.authorization  
+
+    if(!bearer){
+        res.status(401)
+        res.json({ message: "not authorized"})
+        return
+    }
+
+    const [, token] = bearer.split(' ')
+    if(!token){
+        res.status(401)
+        res.json({ message: "not valid token"})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+        return
+    }catch(e){
+        console.error(e)
+        res.status(401)
+        res.json({ message: "not valid token"})
+        return
+    }
+}
